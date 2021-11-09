8393c5a4deade6d8385d02069faa54ea8ba3ac64
Olivier Philippe <olivier.philippe@gmail.com>
2021-11-09 12:32:09 +0100

    broken refactoring data_download

--- dbpgap/data_download.py
+++ dbpgap/data_download.py
@@ -10,61 +10,76 @@ import ftplib
 
 
 
-# Get last version study 
-def extract_number(d):
-    s = re.findall("\d+$",d)
-    return (int(s[0]) if s else -1,d)
-
-source_dir = '/dbgap/studies/' 
-mypath = './data/'
-if not os.path.exists(mypath):
-    os.makedirs(mypath)
 
 
-# init ftp connection
-print('Establishing connection')
-ftp = FTP('ftp.ncbi.nlm.nih.gov', timeout=None)
-ftp.login()
-print('Connection established')
+def get_all_studies(source_dir, ftp_connection):
+    """
+    Get the study folder for all the phs
+    params:
+    source_dir str(): directory were the studies are stored
+    ftp_connection ftplib obj(): connection to the ftp server
 
-# List of all IDs (studies: phs#######)
-all_dbgap_studies = ftp.nlst(source_dir)
+    return:
+    list_all_dbgap_studies list(): list of all the db_gap studies folders
+    """
+    list_all_dbgap_studies = ftp_connection.nlst(source_dir)
+    # Be sure to select the right phs
+    list_all_dbgap_studies = [x for x in list_all_dbgap_studies if 'phs' in x]
+    print('Len of dbgap studies: {}'.format(len(list_all_dbgap_studies)))
+    return list_all_dbgap_studies
 
-print('Len of dbgap studies: {}'.format(len(all_dbgap_studies)))
+def create_study_path(source_dir, phs_name):
+    """
+    Use the name and create the full path from the ftp
+    """
+    def extract_number(d):
+        """ Get last version study """
+        s = re.findall("\d+$",d)
+        return (int(s[0]) if s else -1,d)
 
-# Subsample to test
-# subsample = all_dbgap_studies[1:20]
+    study_path = os.path.join(source_dir, phs_name)
+    study_versions = ftp.nlst(study_path)
+    max_version = max(study_versions, key=extract_number)
+    print(max_version)
+    phenotypes_path = os.path.join(max_version, 'pheno_variable_summaries/')
+    return phenotypes_path
+
+    :
 
 
+if __name__ == "__main__":
 
-for f in all_dbgap_studies:
-    print(f)
-    if 'phs' in f:
-        study_id = os.path.basename(f) 
-        # if not os.path.exists(study_id):
-        #     os.makedirs(study_id)
+    source_dir = '/dbgap/studies/' 
+    mypath = './data/'
+    if not os.path.exists(mypath):
+        os.makedirs(mypath)
 
-        # Phenotype list
-        study_path = os.path.join(source_dir, f)
-        study_versions = ftp.nlst(study_path)
-        max_version = max(study_versions, key=extract_number)
-        print(max_version)
-        phenotypes_path = os.path.join(max_version, 'pheno_variable_summaries/')
-        try:
-            reports = ftp.nlst(phenotypes_path)
-        
-            # Save reports
-            for pht in reports:
-                # I have searched only 'var_report', because 'phenotype'
-                # is not always in the name of the file (to improve)
-                if 'var_report' in pht:
-                    filename = os.path.basename(pht)
-                    print('{}'.format(filename))
-                    with open(mypath+ "/" +filename, 'wb') as fh:
-                        print("dl the file into: {}".format(filename))
-                        ftp.retrbinary('RETR '+pht, fh.write)
-                    break
 
-        except ftplib.error_temp:
-            print('Did not found the file')
-            pass
+    # init ftp connection
+    print('Establishing connection')
+    ftp = FTP('ftp.ncbi.nlm.nih.gov', timeout=None)
+    ftp.login()
+    print('Connection established')
+
+    all_studies_list = get_all_studies(source_dir, ftp)
+
+    phenotypes_path_list = [create_study_path(source_dir, x) for x in all_studies_list]
+
+    try:
+        reports = ftp.nlst(phenotypes_path)
+    
+        # Save reports
+        for pht in reports:
+            # I have searched only 'var_report', because 'phenotype'
+            # is not always in the name of the file (to improve)
+            if 'var_report' in pht:
+                filename = os.path.basename(pht)
+                print('{}'.format(filename))
+                with open(mypath+ "/" +filename, 'wb') as fh:
+                    print("dl the file into: {}".format(filename))
+                    ftp.retrbinary('RETR '+pht, fh.write)
+                break
+
+    except ftplib.error_temp:
+        print('Did not found the file')
+        pass
