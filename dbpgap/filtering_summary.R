ebf87711dd78cf23ec0402c1d9d5c5fe470628d4
AGMAndirko <sanyi.personal@gmail.com>
2021-11-11 15:42:56 +0100

    added dbgap filtering script

---
+++ dbpgap/filtering_summary.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(stringr)
+
+summary <- read_csv("~/Desktop/hackaton/biohackathon-project-35/dbpgap/summary.csv", 
+                    col_names = FALSE, skip = 1)
+
+
+summary <- summary[2:8]
+summary <- as.data.frame(summary)
+summary[1] <- stringr::str_extract(summary[1], "phs.*")
+
+summary <- na.omit(summary)
+
+write.csv(summary, "filtered_summary.csv")
