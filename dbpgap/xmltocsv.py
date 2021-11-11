24dcd8fe180f2d8629ed4673888e2457c8e6768c
Olivier Philippe <olivier.philippe@gmail.com>
2021-11-11 11:01:26 +0100

    progress olivier

--- dbpgap/xmltocsv.py
+++ dbpgap/xmltocsv.py
@@ -4,8 +4,10 @@
 import os
 import re
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ParseError
 import pandas as pd
-
+import lxml.etree as etree
+import glob
 # lists for values
 ids = []
 date = []
@@ -15,7 +17,7 @@ nmale = []
 nfemale = []
 repository = "dbGAP"
 
-xmlpath = "data/"
+xmlpath = "./data/"
 
 # functions
 def parse(xmlpath):
@@ -29,42 +31,60 @@ def minedate(root):
 
 #currently doesn't work 
 def minetotal(root):
-    for child in root.findall('variable'): 
-        for total in child.findall('total'):
-            #for n in stats.find('stat'):
-            print(total.attrib)
-            #for n in total.findall('stat'):
-            #    print(n.attrib)
-            #    totalhere = n.attrib["n"]
-            #   totalcases.append(totalhere)
+    for child in root.findall('*'):
+        for c in child.find("*"):
+        # print(child)
+            print(c.tag, c.attrib, c.text)
+        #for total in child.findall('total'):
+        #    #for n in stats.find('stat'):
+        #    print(total.attrib)
+        #    #for n in total.findall('stat'):
+        #    #    print(n.attrib)
+        #    #    totalhere = n.attrib["n"]
+        #    #   totalcases.append(totalhere)
     
 #parse and retrieve dates
-for xml in os.listdir(xmlpath):
-    ids.append(os.path.splitext(xml))
-    if xml.endswith(xml):
-        
+n = 0
+for file in glob.glob('./data/*.xml'):
+    # context = etree.iterparse(file, tag='*', events = ('end', ))
+    # print(context)
+    try:
+        root = parse(file)
         try:
-            root = parse(xmlpath+xml)
-        except:
-            date.append("NA")
-            continue
-            
-        try:
-            minedate(root)
-        except:
-            date.append("NA")
-            
-        #works up until this function:
-        minetotal(root)
+            print(file)
+            for child in root.iter():
+                for c in child.findall('sex'):
+                    for i in c.iter():
+                        print(i.tag, i.attrib, i.text)
+                    # print(c.tag, c.attrib, c.text)
+                # for c in child.findall('male'):
+                #     print(c.tag, c.attrib, c.text)
+                # for c in child.findall('female'):
+                #     print(c.tag, c.attrib, c.text)
 
+            n+=1
+        except TypeError:
+            pass
 
-#summary
-print(len(ids)) #ok
-print(len(date)) #ok w/ some error handling 
-print(len(totalcases)) # empty at the moment
-print(len(nulls)) # empty at the moment
-print(len(nfemale)) # empty at the moment
-print(len(nmale)) # empty at the moment
+            # print(child.tag, child.attrib, child.text)
+    except ParseError: 
+        pass
+    
+        #try:
+        #    root = parse(xmlpath+xml)
+        #except:
+        #    date.append("NA")
+        #    continue
+            
+        #try:
+        #    minedate(root)
+        #except:
+        #    date.append("NA")
+            
+        ##works up until this function:
+    # minetotal(root)
+    if n == 10:
+        break
 
 
 #df = pd.DataFrame({'Study_ids': ids, 'Date': date, 'Total': totalcases, 'Nulls': nulls, 'Female': nfemale, 'Male': nmale, 'Repository': "dbgap"})
