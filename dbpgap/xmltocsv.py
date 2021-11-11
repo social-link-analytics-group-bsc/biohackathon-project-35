ce917245d3001e7a2edefb8de65afc74e73b068b
AGMAndirko <sanyi.personal@gmail.com>
2021-11-11 12:05:48 +0100

    max function to sex, total cases, nulls

--- dbpgap/xmltocsv.py
+++ dbpgap/xmltocsv.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import os
 import re
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ParseError
 import pandas as pd
-import lxml.etree as etree
+import xml.etree as etree
 import glob
 # lists for values
 ids = []
@@ -16,19 +15,15 @@ nulls = []
 nmale = []
 nfemale = []
 repository = "dbGAP"
-
 xmlpath = "./data/"
-
 # functions
 def parse(xmlpath):
     phs = ET.parse(xmlpath)
     root = phs.getroot()
     return(root)
-
 def minedate(root):
     datehere = root.attrib["date_created"]
     date.append(datehere)
-
 #currently doesn't work 
 def minetotal(root):
     for child in root.findall('*'):
@@ -45,28 +40,59 @@ def minetotal(root):
     
 #parse and retrieve dates
 n = 0
+
+
 for file in glob.glob('./data/*.xml'):
+    maxmale = []
+    maxfemale = []
+    maxn = []
+    maxnull = []
     # context = etree.iterparse(file, tag='*', events = ('end', ))
     # print(context)
     try:
         root = parse(file)
         try:
             print(file)
+            # sex
             for child in root.iter():
                 for c in child.findall('sex'):
                     for i in c.iter():
-                        print(i.tag, i.attrib, i.text)
-                    # print(c.tag, c.attrib, c.text)
-                # for c in child.findall('male'):
-                #     print(c.tag, c.attrib, c.text)
-                # for c in child.findall('female'):
-                #     print(c.tag, c.attrib, c.text)
 
+                        if i.tag == 'male':
+                            #print(i.tag, i.text)
+                            nmale = int(i.text)
+                            maxmale.append(nmale)
+
+                        if i.tag == 'female':
+                            nfemale = int(i.text)
+                            maxfemale.append(nfemale)
+            # total cases 
+            for child in root.iter():
+                for c in child.findall('stat'):
+                    for i in c.iter():
+                        ntotal = i.attrib['n']
+                        ntotal = int(ntotal)
+                        maxn.append(ntotal)
+
+            # null cases
+            for child in root.iter():
+                for c in child.findall('stat'):
+                    for i in c.iter():
+                        nulltotal = i.attrib['nulls']
+                        nulltotal = int(nulltotal)
+                        maxnull.append(nulltotal)
             n+=1
+            try:
+                print(max(maxmale))
+                print(max(maxfemale))
+                print(max(maxn))
+                #print(max(maxnull))
+            except:
+                print("NA")
         except TypeError:
             pass
-
             # print(child.tag, child.attrib, child.text)
+
     except ParseError: 
         pass
     
@@ -86,6 +112,3 @@ for file in glob.glob('./data/*.xml'):
     if n == 10:
         break
 
-
-#df = pd.DataFrame({'Study_ids': ids, 'Date': date, 'Total': totalcases, 'Nulls': nulls, 'Female': nfemale, 'Male': nmale, 'Repository': "dbgap"})
-#df.to_csv("summary.csv")
