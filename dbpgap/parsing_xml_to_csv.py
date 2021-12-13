49957765a59b9c456caf1290d575d7bb82aee979
Diego S <cuquiwi@hotmail.com>
2021-12-13 16:29:20 +0100

    Adds parsing for SUBJECT_ID and SAMPLE_ID to dbgap parsing

--- dbpgap/parsing_xml_to_csv.py
+++ dbpgap/parsing_xml_to_csv.py
@@ -16,6 +16,23 @@ def parse(xmlpath):
     root = phs.getroot()
     return(root)
 
+def parse_data(tree,dict_to_save,pre_tag=None):
+    for c in tree.iter('sex'):
+        for a in c:
+            if pre_tag:
+                dict_to_save.setdefault(pre_tag+a.tag, []).append(a.text)
+            else:
+                dict_to_save.setdefault(a.tag, []).append(a.text)
+
+
+    for i in tree.iter('stat'):
+        info = i.attrib
+        for key in info:
+            if pre_tag:
+                dict_to_save.setdefault(pre_tag+key, []).append(info[key])
+            else:
+                dict_to_save.setdefault(key, []).append(info[key])
+
 n = 0
 good_files_count = 0
 none_type_error_count = 0
@@ -34,21 +51,19 @@ for file in glob.glob('./data/*.xml'):
         # Get the date
 
         dict_data['date'] = root.attrib["date_created"]
+        dict_data['dataset_id'] = root.attrib['dataset_id']
         # date.append(NA)
         dict_data['filename'] = file
         for child in root.iter('variable'):
             if re.search(pattern, child.attrib['var_name']):
-                for c in child.iter('sex'):
-                    for a in c:
-                        dict_data.setdefault(a.tag, []).append(a.text)
+                parse_data(child,dict_data)
 
+            if child.attrib['var_name'] == 'SUBJECT_ID':
+                parse_data(child,dict_data,pre_tag="subject_")
 
+            if child.attrib['var_name'] == 'SAMPLE_ID':
+                parse_data(child,dict_data,pre_tag="sample_")
 
-        
-                for i in child.iter('stat'):
-                    info = i.attrib
-                    for key in info:
-                        dict_data.setdefault(key, []).append(info[key])
         # Getting the max_value for each key if contains a list
         for k in dict_data:
             if isinstance(dict_data[k], list):
@@ -63,7 +78,7 @@ for dict_ in list_dict:
         list_keys.append(k)
 keys = set(list_keys)
 print(keys)
-with open('summary_second.csv', 'w') as output_file:
+with open('summary_third.csv', 'w') as output_file:
     dict_writer = csv.DictWriter(output_file, fieldnames=keys, delimiter=',')
     dict_writer.writeheader()
     dict_writer.writerows(list_dict)
