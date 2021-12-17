6eb4e9596d9cdc30191037e56ad47eb0bea3ff20
Diego S <cuquiwi@hotmail.com>
2021-12-17 15:50:47 +0100

    summary dbgap fourth fix

--- dbpgap/summary_data.py
+++ dbpgap/summary_data.py
@@ -31,8 +31,9 @@ print(len(d2))
 d1['n'] = d1['n'].fillna(d1.sample_n).fillna(d1.subject_n)
 d1['male'] = d1['male'].fillna(d1.sample_male).fillna(d1.subject_male).fillna(0)
 d1['female'] = d1['female'].fillna(d1.sample_female).fillna(d1.subject_female).fillna(0)
-d1['unknown'] = d1.n - d1.male + d1.female
-d1 = d1.loc[d1.unknown >=0]
+d1['unknown'] = d1.n - (d1.male + d1.female)
+d1 = d1.loc[d1.unknown >=0] 
+
 print(d1.loc[d1.unknown ==0].shape)
 
 d1.loc[:,['n','male', 'female','unknown', 'date']].to_csv('summary_fourth.csv')
