706f2535fab3f9992d64b974e4289a775069706d
vicruiser <victoria.ruiz.serra@gmail.com>
2022-02-28 08:26:06 +0100

    fixed code fig 3

--- code_figures/figure3.R
+++ code_figures/figure3.R
@@ -6,21 +6,14 @@ library(dplyr)
 library(lubridate)
 
 # Load data
-ega = fread("/media/victoria/VICTORIA_EXTERNAL_DISK/BioHackathon2021/EGA_examples/EGA_with_NULL.csv")
-ega$year = str_split_fixed(ega$to_char, '-', 2)[,1]
-ega_m = melt(ega, id.vars = c("ega_stable_id", "repository", "to_char", "year", "total"))
-
-colnames(ega_m)[colnames(ega_m) == "ega_stable_id"] = "dataset_id"
-
-
-ega = fread("/media/victoria/VICTORIA_EXTERNAL_DISK/BioHackathon2021/EGA_examples/all_EGA_samples.txt")
+ega = fread("../ega/all_EGA_samples.txt")
 #ega$total_all = length(unique(ega$ega_stable_id))
 ega$year = str_split_fixed(ega$date_study, '-', 2)[,1]
 colnames(ega)[1] = "dataset_id"
 ega_m = melt(ega, id.vars = c("dataset_id", "repository", "date_study", "year", "total"))
 ega_m$year[ega_m$year=="1980"] = "NA"
 
-dbgap = fread("/media/victoria/VICTORIA_EXTERNAL_DISK/BioHackathon2021/biohackathon-project-35/dbpgap/summary_fourth.csv")
+dbgap = fread("../dbpgap/summary_fourth.csv")
 dbgap$total = dbgap$male +  dbgap$female +  dbgap$unknown
 dbgap$date = parse_date_time(dbgap$date, orders = c('mdy', 'dmy','ymd', "%d %m &y %H:%M:%S %Y"))
 dbgap$year =str_split_fixed(dbgap$date, '-', 2)[,1]
@@ -39,20 +32,6 @@ ega_dbgap_percent = subset(ega_dbgap) %>% group_by(variable, repository, year) %
   summarise(value_percent = sum(value) / sum(total))
 ega_dbgap_percent$year = factor(ega_dbgap_percent$year ,
                                  levels = c('NA','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'))
-
-# sample_plot_percent = ggplot(subset(ega_dbgap_percent), aes(x=variable, y = value_percent, fill=variable))+
-#   geom_bar(stat="identity", width= 0.8)+
-#   ylab("Percentage of samples")+
-#   xlab("Biological sex specification")+
-#   theme_minimal()+
-#   scale_y_continuous(labels=scales::percent) +
-#   facet_grid(repository~year)+
-#   scale_fill_manual(values = c("#ffa600", "#bc5090","#003f5c"))+
-#   scale_x_discrete(labels= c("M", "F", "U"))+
-#   theme(legend.position = "none",
-#         axis.text.x = element_text(size = 10),
-#         strip.text = element_text(size = 16, face = "bold"))
-
 
 sample_plot_percent = ggplot(subset(ega_dbgap_percent), aes(x=year, y = value_percent, color=variable, group=variable))+
   #geom_bar(stat="identity", width= 0.8)+
