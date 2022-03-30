250d72130ebbe99098a5fac9ad984aef25f70b7c
vicruiser <victoria.ruiz.serra@gmail.com>
2022-03-30 10:46:37 +0200

    updated figures

--- code_figures/figure2.R
+++ code_figures/figure2.R
@@ -3,25 +3,17 @@ library(reshape2)
 library(ggplot2)
 library(stringr)
 library(dplyr)
-library(lubridate)
+
 # Load data
 ega = fread("../ega/EGA_with_NULL.csv")
 ega$total_all = length(unique(ega$ega_stable_id))
-ega$year = str_split_fixed(ega$to_char, '-', 2)[,1]
-
+#d2 = melt(d,id.vars = c("ega_stable_id", "repository", "to_char", "total"))
 dbgap = fread("../dbpgap/summary_fourth.csv")
 dbgap$total = dbgap$male +  dbgap$female +  dbgap$unknown
-dbgap$date = parse_date_time(dbgap$date, orders = c('mdy', 'dmy','ymd', "%d %m &y %H:%M:%S %Y"))
-dbgap$year =str_split_fixed(dbgap$date, '-', 2)[,1]
-dbgap$repository = "dbGaP"
-
-dbgap$year[dbgap$year==""] = "NA"
-dbgap = subset(dbgap, year > 2017)
+dbgap$repository="dbGaP"
 dbgap$total_all = length(unique(dbgap$dataset_id))
-
-dbgap$phe = substr(dbgap$filename, 8,19)
-
 ega_dbgap = rbind(ega,dbgap, fill = T)
+
 ################
 # study level  #
 ################
@@ -49,7 +41,6 @@ female_and_male_and_unknown$label = 'F&M&U'
 
 r = rbind(unknown_only, female_only, male_only, female_and_male, female_and_unknown, male_and_unknown, female_and_male_and_unknown)
 r $ c = 1
-write.table(r, 'ega_dbgap_studies_classification.txt', sep = "\t")
 # percent
 ega_dbgap_percent = unique(r %>% group_by(label, repository) %>%
   summarise(value_percent = sum(c) / total_all))
@@ -58,11 +49,12 @@ ega_dbgap_percent$label = factor(ega_dbgap_percent$label, levels = unique(f))
 
 study_plot_percent = ggplot(ega_dbgap_percent, aes(x= label,y = value_percent, fill = label))+
   geom_bar(stat="identity", width=0.75)+
-  ylab("Percentage studies")+
+  ylab("Percentage of EGA studies")+
   xlab("Sex specification in samples found in the study")+
   theme_minimal()+
   facet_grid(~repository)+
   scale_y_continuous(labels=scales::percent) +
+  scale_fill_brewer(palette = "Set2")+
   theme(legend.position = "none",
         axis.text.x = element_text(size = 10, face = "bold"),
        # axis.text.x = element_text(size = 12),
@@ -73,37 +65,3 @@ png('../figures/figure2.png',
 study_plot_percent
 dev.off()
 
-
-
-#### phenotype
-
-ega_phe =fread("/media/victoria/VICTORIA_EXTERNAL_DISK/BioHackathon2021/EGA_examples/raw_phenotype_gender_ega.txt")
-ega_phe_u = subset(ega_phe , ega_study %in% unknown_only$ega_stable_id)
-ega_phe_u $c = 1
-ega_phe_u_total = ega_phe_u%>%group_by(phenotype) %>% summarise(total = sum(c))
-arrange(ega_phe_u_total, desc(total))
-top20 = subset(ega_phe_u_total,total> 720)
-top20$phenotype = factor(top20$phenotype, levels = arrange(top20, desc(total))$phenotype)
-ggplot(top20, aes(x = phenotype, y = total))+
-  geom_bar(stat="identity")+
-  theme_minimal()+
-  theme(axis.text.x = element_text(size = 10, face = "bold", angle = 45, hjust = 1, vjust = 1),
-        # axis.text.x = element_text(size = 12),
-        strip.text = element_text(size = 16, face = "bold"))
-
-
-
-dbgap_phe =fread("/media/victoria/VICTORIA_EXTERNAL_DISK/BioHackathon2021/biohackathon-project-35/dbpgap/archived/summary_primary_phenotype.tsv")
-dbgap_phe$dataset_id = substr(dbgap_phe$`study accession`, 1, 12)
-dbgap_phe_fmu = subset(dbgap_phe , dataset_id %in% female_and_male_and_unknown$phe)
-dbgap_phe_fmu $c = 1
-dbgap_phe_fmu_total = dbgap_phe_fmu%>%group_by(`primary phenotype`) %>% summarise(total = sum(c))
-arrange(dbgap_phe_fmu_total, desc(total))
-top20 = subset(dbgap_phe_fmu_total,total>25)
-top20$`primary phenotype` = factor(top20$`primary phenotype`, levels = arrange(top20, desc(total))$`primary phenotype`)
-ggplot(top20, aes(x = `primary phenotype`, y = total))+
-  geom_bar(stat="identity")+
-  theme_minimal()+
-  theme(axis.text.x = element_text(size = 10, face = "bold", angle = 45, hjust = 1, vjust = 1),
-        # axis.text.x = element_text(size = 12),
-        strip.text = element_text(size = 16, face = "bold"))
--- code_figures/figure3.R
+++ code_figures/figure3.R
@@ -33,30 +33,13 @@ ega_dbgap = ega_dbgap %>% group_by(repository, year) %>% mutate(total_year = sum
 ################
 # sample level #
 ################
+
 ega_dbgap_percent = subset(ega_dbgap) %>% group_by(variable, repository, year) %>%
   summarise(value_percent = sum(value) / sum(total),
             total_year = unique(total_year))
 ega_dbgap_percent$year = factor(ega_dbgap_percent$year ,
                                  levels = c('NA','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'))
 
-
-# sample_plot_percent = ggplot(subset(ega_dbgap_percent), aes(x=year,  color=variable, group=variable))+
-#  # geom_line(aes(y = total_year),fill = "grey30", stat="identity", width= 0.8)+
-#   geom_point(aes(y = value_percent), size = 3)+
-#   geom_line(aes(y = value_percent))+
-#   ylab("Percentage of samples")+
-#   xlab("Year")+
-#   theme_minimal()+
-#   scale_y_continuous(labels=scales::percent)+#,
-#                      #sec.axis = sec_axis(~.*0.01, name="Second Axis")) +
-#   facet_grid(repository~.)+
-#   scale_color_manual(values = c("#ffa600", "#bc5090","#003f5c"),
-#                      labels = c("Male", "Female", "Unknown"),
-#                      name ="")+
-#   #scale_x_discrete(labels= c("M", "F", "U"))+
-#   theme(legend.position = "top",
-#         axis.text.x = element_text(size = 10),
-#         strip.text = element_text(size = 16, face = "bold"))
 
 ega1 = ggplot(subset(ega_dbgap_percent, repository == "EGA"),
               aes(x=year,  group=repository))+
@@ -125,7 +108,6 @@ dbgap2 = ggplot(subset(ega_dbgap_percent, repository =="dbGaP"),
 
 
 
-
 pp = plot_grid(dbgap1, dbgap2,ega1, ega2, labels = c('A', 'B', "C", "D"), ncol = 1, rel_heights = c(0.25,0.5, 0.25, 0.5))
 
 png('../figures/figure3.png',
@@ -133,3 +115,117 @@ png('../figures/figure3.png',
 pp
 dev.off()
 
+### add studies classification
+library(data.table)
+library(reshape2)
+library(ggplot2)
+library(stringr)
+library(dplyr)
+library(lubridate)
+# Load data
+ega = fread("../ega/all_EGA_samples.txt")
+
+ega$year = str_split_fixed(ega$date_study, '-', 2)[,1]
+colnames(ega)[1] = "dataset_id"
+ega = ega %>% group_by(year) %>%
+  mutate(total_all = length(unique(dataset_id)))
+
+dbgap = fread("../dbpgap/summary_fourth.csv")
+dbgap$total = dbgap$male +  dbgap$female +  dbgap$unknown
+dbgap$date = parse_date_time(dbgap$date, orders = c('mdy', 'dmy','ymd', "%d %m &y %H:%M:%S %Y"))
+dbgap$year =str_split_fixed(dbgap$date, '-', 2)[,1]
+dbgap$repository = "dbGaP"
+
+dbgap$year[dbgap$year==""] = "NA"
+#dbgap = subset(dbgap, year > 2017)
+dbgap = dbgap %>% group_by(year) %>%
+  mutate(total_all = length(unique(dataset_id)))
+
+
+dbgap$phe = substr(dbgap$filename, 8,19)
+
+ega_dbgap = rbind(ega,dbgap[colnames(ega[,-6])])
+################
+# study level  #
+################
+unknown_only = subset(ega_dbgap, male == 0 & female == 0)
+unknown_only$label = "U"
+
+female_only = subset(subset(ega_dbgap, male == 0 & unknown == 0))
+female_only$label = 'F'
+
+male_only = subset(ega_dbgap, female == 0 & unknown == 0)
+male_only$label = 'M'
+
+female_and_male = subset(ega_dbgap, female != 0 & male != 0 & unknown == 0)
+female_and_male$label = 'F&M'
+
+female_and_unknown = subset(ega_dbgap, female != 0 & male == 0 & unknown != 0)
+female_and_unknown$label = 'F&U'
+
+male_and_unknown = subset(ega_dbgap, female == 0 & male != 0 & unknown != 0)
+male_and_unknown$label = 'M&U'
+
+female_and_male_and_unknown = subset(ega_dbgap, female != 0 & male != 0 & unknown != 0)
+female_and_male_and_unknown$label = 'F&M&U'
+
+
+r = rbind(unknown_only, female_only, male_only, female_and_male, female_and_unknown, male_and_unknown, female_and_male_and_unknown)
+r $ c = 1
+#write.table(r, 'ega_dbgap_studies_classification.txt', sep = "\t")
+# percent
+ega_dbgap_percent = unique(r %>% group_by(label, repository,year) %>%
+                             summarise(value_percent = sum(c) / total_all))
+f = arrange(ega_dbgap_percent, desc(value_percent))$label
+ega_dbgap_percent$label = factor(ega_dbgap_percent$label, levels = c("F&M", "F&M&U", "U", "F" , "M", "F&U" ,"M&U"))
+
+ega_dbgap_percent$year[ega_dbgap_percent$year == 1980] = "NA"
+ega_dbgap_percent$year = factor(ega_dbgap_percent$year,
+                                levels = c('NA','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'))
+
+cbp1 <- c( "#E69F00", "#56B4E9", "#009E73",
+           "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+dbgap3 = ggplot(subset(ega_dbgap_percent, repository == "dbGaP"),
+                aes(x= year, color = label, group =label ))+
+  geom_point(aes(y = value_percent), size = 3)+
+  geom_line(aes(y = value_percent))+
+  ylab("Percentage studies")+
+  xlab("Sex specification in samples found in the study")+
+  theme_minimal()+
+  facet_grid(repository~., scales= "free_y")+
+  #scale_color_manual(values = cbp1)+
+  scale_color_brewer(palette = "Set2")+
+  theme(axis.text.x = element_text(size = 10),
+        legend.position="top",
+        legend.box = "horizontal",
+        strip.text = element_text(size = 16, face = "bold"))+
+  guides(color=guide_legend(nrow=1,byrow=TRUE, title = "Study classification"))
+
+
+ega3 = ggplot(subset(ega_dbgap_percent, repository == "EGA"),
+                aes(x= year, color = label, group =label ))+
+  geom_point(aes(y = value_percent), size = 3)+
+  geom_line(aes(y = value_percent))+
+  ylab("Percentage studies")+
+  xlab("Sex specification in samples found in the study")+
+  theme_minimal()+
+  facet_grid(repository~., scales= "free_y")+
+  #scale_color_manual(values = cbp1)+
+  scale_color_brewer(palette = "Set2")+
+  theme(axis.text.x = element_text(size = 10),
+        legend.position="top",
+        legend.box = "horizontal",
+        strip.text = element_text(size = 16, face = "bold"))+
+  guides(color=guide_legend(nrow=1,byrow=TRUE, title = "Study classification"))
+
+
+pp = plot_grid(dbgap1, dbgap2,dbgap3, ega1, ega2,ega3,
+               labels = c('A', 'B', "C", "D","E", "F"),
+               ncol = 1, rel_heights = c(0.25,0.5,0.5, 0.25, 0.5,0.5))
+pp
+
+png('../figures/figure3_alternative.png',
+    width = 1500, height = 2000, res = 150)
+pp
+dev.off()
